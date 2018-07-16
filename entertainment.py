2aedcee5c8d95a6259c7ee0bb6df9804a8b28c21
MoatazBellah <motazbellahhamdy@gmail.com>
2018-07-16 23:32:07 +0200

    Create folder for poster image and change the link

--- entertainment.py
+++ entertainment.py
@@ -3,12 +3,12 @@ import fresh_tomatoes
 
 interstaller = media.Movie("Interstaller",
                             "Man search for a new planet",
-                            "interstaller.jpg",
+                            "poster_image/interstaller.jpg",
                             "https://www.youtube.com/watch?v=0vxOhd4qlnA")
 
 shawshank_redemption = media.Movie("Shawshank Redemption",
                                     "About an honst man that sent to Shawshank Prison, then he could escaped from it",
-                                    "Shawshank.jpg",
+                                    "poster_image/Shawshank.jpg",
                                     "https://www.youtube.com/watch?v=6hB3S9bIaco")
 
 avatar = media.Movie("Avatar",
@@ -18,17 +18,17 @@ avatar = media.Movie("Avatar",
 
 theory_of_everything = media.Movie("The theory of everything",
                                    "True story about Stephen Hawking",
-                                   "The_Theory_of_Everything.jpg",
+                                   "poster_image/The_Theory_of_Everything.jpg",
                                    "https://www.youtube.com/watch?v=Salz7uGp72c")
 
 beautiful_mind = media.Movie("A beautiful mind",
                              "True story about John Nash",
-                             "beautiful_mind.jpg",
+                             "poster_image/beautiful_mind.jpg",
                              "https://www.youtube.com/watch?v=WFJgUm7iOKw")
 
 school_of_rock = media.Movie("School of Rock",
                              "Using Rock music to learn",
-                             "school.jpg",
+                             "poster_image/school.jpg",
                              "https://www.youtube.com/watch?v=3PsUJFEBC74")
 
 movies = [interstaller, shawshank_redemption, avatar, theory_of_everything, beautiful_mind, school_of_rock]
